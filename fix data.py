9bdc4aa959231939c2bb1a7381fb10ff95c53766
Penomax <haqiqi.reza@gmail.com>
2020-03-06 13:43:24 +0330

    hale

--- fix data.py
+++ fix data.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import time
+from collections import defaultdict
 from io import StringIO
 import numpy as np
 import pandas as pd
@@ -12,31 +13,53 @@ with open('Team40_10FIXTURE_BY_ID.json') as f:
 
 
 statics=[]
-stLIst={} #primary list for complite statcs dict
 
-for x in range (10):
+
+
+for x in range (len(data["FixtureByID"])):
+    stLIst={} #Inner dict
     print(x)
-    if data["FixtureByID"][x]["api"]["fixtures"][0]["homeTeam"]["team_id"] == 40 :
-
-        stLIst["fixtureId"  ] = data["FixtureByID"][x]["api"]["fixtures"][0]["fixture_id"]                           
-        stLIst["shotOnGoal" ] = data["FixtureByID"][x]["api"]["fixtures"][0]["statistics"]["Shots on Goal"]["home"]  
-        stLIst["shotOffGoal"] = data["FixtureByID"][x]["api"]["fixtures"][0]["statistics"]["Shots off Goal"]["home"]  
-        stLIst["totalGoal"  ] = data["FixtureByID"][x]["api"]["fixtures"][0]["statistics"]["Total Shots"]["home"]
-        statics.append(stLIst)     
+    if data["FixtureByID"][x]["api"]["fixtures"][0]["statistics"] is not None:
+        print("true")
+        if data["FixtureByID"][x]["api"]["fixtures"][0]["homeTeam"]["team_id"] is 40 :
+            
+            stLIst["fixtureId"  ] = int( data["FixtureByID"][x]["api"]["fixtures"][0]["fixture_id"]                           )
+            stLIst["shotOnGoal" ] = int( data["FixtureByID"][x]["api"]["fixtures"][0]["statistics"]["Shots on Goal"]["home"] )
+            stLIst["shotOffGoal"] = int( data["FixtureByID"][x]["api"]["fixtures"][0]["statistics"]["Shots off Goal"]["home"]  )
+            stLIst["totalGoal"  ] = int( data["FixtureByID"][x]["api"]["fixtures"][0]["statistics"]["Total Shots"]["home"])
+            statics.append(stLIst)
+            # print(json.dumps(statics,indent=4) )
+            # time.sleep(10)     
+            
+        else:
+            
+            stLIst["fixtureId"  ] = int( data["FixtureByID"][x]["api"]["fixtures"][0]["fixture_id"]                           )     
+            stLIst["shotOnGoal" ] = int( data["FixtureByID"][x]["api"]["fixtures"][0]["statistics"]["Shots on Goal"]["away"]  )   
+            stLIst["shotOffGoal"] = int( data["FixtureByID"][x]["api"]["fixtures"][0]["statistics"]["Shots off Goal"]["away"] )   
+            stLIst["totalGoal"  ] = int( data["FixtureByID"][x]["api"]["fixtures"][0]["statistics"]["Total Shots"]["away"]    )   
+            statics.append(stLIst)
+            # print(json.dumps(statics,indent=4) )
+            # time.sleep(10)
     else:
-        pass
-        stLIst["fixtureId"  ] = data["FixtureByID"][x]["api"]["fixtures"][0]["fixture_id"]                                
-        stLIst["shotOnGoal" ] = data["FixtureByID"][x]["api"]["fixtures"][0]["statistics"]["Shots on Goal"]["away"]     
-        stLIst["shotOffGoal"] = data["FixtureByID"][x]["api"]["fixtures"][0]["statistics"]["Shots off Goal"]["away"]    
-        stLIst["totalGoal"  ] = data["FixtureByID"][x]["api"]["fixtures"][0]["statistics"]["Total Shots"]["away"]       
+        print("false")
+        stLIst["fixtureId"  ] = data["FixtureByID"][x]["api"]["fixtures"][0]["fixture_id"]
+        stLIst["shotOnGoal" ] = data["FixtureByID"][x]["api"]["fixtures"][0]["statistics"]
+        stLIst["shotOffGoal"] = data["FixtureByID"][x]["api"]["fixtures"][0]["statistics"] 
+        stLIst["totalGoal"  ] = data["FixtureByID"][x]["api"]["fixtures"][0]["statistics"] 
         statics.append(stLIst)
-
-print(statics)
-
-with open('test&test2.json', "a") as ff:
-        json.dump(statics, ff)
+        # print(json.dumps(statics,indent=4) )
+        # time.sleep(10)
 
 
 
+with open('finalyRight.json', "w+") as ff:
+    # ff.truncate()
+    ff.flush()
+    json.dump(statics,ff)
 
 
+
+# print(json.dumps(statics,indent=4) )
+
+
+# print(len(data["FixtureByID"]))
