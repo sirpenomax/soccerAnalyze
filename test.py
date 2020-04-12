1e4d7a0597cc0e4828fe74363d893dad2500535b
Penomax <haqiqi.reza@gmail.com>
2020-04-12 23:41:05 +0430

    fdgdfgbgfbfghfn

--- test.py
+++ test.py
@@ -10,46 +10,28 @@ import numpy
 import matplotlib.pyplot as plt
 import sys
 import csv
-import datetime
+# import datetime
 logging.basicConfig(level=logging.DEBUG)
 import COVID19Py
 import matplotlib
 
+import googlemaps
+from datetime import datetime
 
-labels = ['G1', 'G2', 'G3', 'G4', 'G5']
-men_means = [20, 34, 30, 35, 27]
-women_means = [25, 32, 34, 20, 25]
+from bokeh.plotting import figure, output_file, show
 
-x = np.arange(len(labels))  # the label locations
-width = 0.35  # the width of the bars
+# prepare some data
+x = [1, 2, 3, 4, 5]
+y = [6, 7, 2, 4, 5]
 
-fig, ax = plt.subplots()
-rects1 = ax.bar(x - width/2, men_means, width, label='Meno')
-rects2 = ax.bar(x + width/2, women_means, width, label='Women')
-print(rects1)
+# output to static HTML file
+output_file("lines.html")
 
-# Add some text for labels, title and custom x-axis tick labels, etc.
-ax.set_ylabel('Scores')
-ax.set_title('Scores by group and gender')
-ax.set_xticks(x)
-ax.set_xticklabels(labels)
-ax.legend()
+# create a new plot with a title and axis labels
+p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
 
+# add a line renderer with legend and line thickness
+p.line(x, y, line_width=2)
 
-def autolabel(rects):
-    """Attach a text label above each bar in *rects*, displaying its height."""
-    for rect in rects:
-        height = rect.get_height()
-        ax.annotate('{}'.format(height),
-                    xy=(rect.get_x() + rect.get_width() / 2, height),
-                    xytext=(0, 3),  # 3 points vertical offset
-                    textcoords="offset points",
-                    ha='center', va='bottom')
-
-
-autolabel(rects1)
-autolabel(rects2)
-
-fig.tight_layout()
-
-plt.show()
+# show the results
+show(p)
